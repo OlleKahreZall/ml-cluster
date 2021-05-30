466425ce4478d737b7cea1098af10ba7bb68ecb2
OlleKahreZall <72204060+OlleKahreZall@users.noreply.github.com>
2021-05-30 17:35:46 +0200

    Update NN.py

--- training/NN.py
+++ training/NN.py
@@ -11,7 +11,7 @@ from os import path
 # reading the csv file using read_csv
 # storing the data frame in variable called df
 basepath = path.dirname(__file__)
-df = pd.read_csv(basepath + '/datasets/final_dataset_train.csv')
+df = pd.read_csv(basepath + 'datasets/final_dataset_train.csv')
 
 # creating a list of column names by calling the .columns
 features = list(df.columns)
